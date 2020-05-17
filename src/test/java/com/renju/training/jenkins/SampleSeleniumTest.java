abbc38279e2db6098a1605084e54a6cad500c1eb
KalyansGithub <43195816+KalyansGithub@users.noreply.github.com>
2020-05-17 13:47:25 +0530

    Update SampleSeleniumTest.java

--- src/test/java/com/renju/training/jenkins/SampleSeleniumTest.java
+++ src/test/java/com/renju/training/jenkins/SampleSeleniumTest.java
@@ -15,7 +15,7 @@ public class SampleSeleniumTest {
 	
 	@BeforeClass
 	public void startBrowser() {
-		System.setProperty("webdriver.chrome.driver", "D:\\Professional\\Softwares\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Kalyan\\Downloads\\chromedriver_win32 (2)\\chromedriver.exe");
 		ChromeOptions chromeOptions = new ChromeOptions();
 		chromeOptions.addArguments("--start-maximized");
 		driver = new ChromeDriver(chromeOptions);
